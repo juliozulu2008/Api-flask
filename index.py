a76b9c420cdcec1ee098b199c8c06e1423e543d4
Julio Cesar <juliozulu2008@gmail.com>
2023-11-17 22:16:55 -0300

    sql alquemy

--- index.py
+++ index.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
 
-@app.route("/", methods=["GET"])
-def index():
-    return "Hello World!", 200
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
+class UserResource(Resource):
+    def get(self):
+        users = User.query.all()
+        return [{'id': user.id, 'username': user.username, 'email': user.email} for user in users]
+
+    def post(self):
+        data = request.get_json()
+        new_user = User(username=data['username'], email=data['email'])
+        db.session.add(new_user)
+        db.session.commit()
+        return {'message': 'User created successfully'}, 201
+
+api.add_resource(UserResource, '/users')
 
 if __name__ == '__main__':
-    app.run()
+    db.create_all()
+    app.run(debug=True)
